b36387d60a14734c5061466d4a1e4aa2c44f4492
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2024-03-31 15:46:45 +0300

    added notifications to the app

--- ShoppingList/src/components/Notifications.tsx
+++ ShoppingList/src/components/Notifications.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { View } from 'react-native';
-import { Card, Switch, Button, Paragraph,Title,Divider } from 'react-native-paper';
+import { Card, Switch, Button, Paragraph, Title, Divider } from 'react-native-paper';
+import * as Notifications1 from 'expo-notifications';
+
 // component to display the notifications settings
 export const Notifications = () => {
   const [isSwitchOn, setIsSwitchOn] = useState(false);
@@ -10,22 +12,37 @@ export const Notifications = () => {
   const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
   const onToggleOutdatedSwitch = () => setIsOutdatedSwitchOn(!isOutdatedSwitchOn);
 
+  const saveSettings = async () => {
+    // save your settings here
+
+    // send a notification
+    if (isSwitchOn) {
+      await Notifications1.scheduleNotificationAsync({
+        content: {
+          title: "Settings Saved!",
+          body: 'Your notification settings have been saved.',
+        },
+        trigger: null,
+      });
+    }
+  };
+
   return (
     <View style={{margin:10}}>
       <Title style={{fontSize: 30, fontWeight: 'bold', alignSelf: 'center',marginBottom: 5}}>Notifications</Title>
         <Divider />
-    <Card>
-      <Card.Title title="Notifications Settings" />
-      <Card.Content>
-        <Paragraph>Allow Notifications</Paragraph>
-        <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
-        <Paragraph>Allow Notifications for Outdated Items</Paragraph>
-        <Switch value={isOutdatedSwitchOn} onValueChange={onToggleOutdatedSwitch} />
-      </Card.Content>
-      <Card.Actions>
-        <Button onPress={() => {}}>Save</Button>
-      </Card.Actions>
-    </Card>
+      <Card>
+        <Card.Title title="Notifications Settings" />
+        <Card.Content>
+          <Paragraph>Allow Notifications</Paragraph>
+          <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
+          <Paragraph>Allow Notifications for Outdated Items</Paragraph>
+          <Switch value={isOutdatedSwitchOn} onValueChange={onToggleOutdatedSwitch} />
+        </Card.Content>
+        <Card.Actions>
+          <Button onPress={saveSettings}>Save</Button>
+        </Card.Actions>
+      </Card>
     </View>
   );
 };
